03362e36fb38ed2118ab87db9cadf46f6b763ad2
koo0ki <kakoytoaleksandr@gmail.com>
2025-02-23 16:21:07 +0700

    main page

--- views/assets/script.js
+++ views/assets/script.js
@@ -13,3 +13,50 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
+const getDate = () => {
+    const date = new Date();
+    const months = {
+        'Jan': 'янв',
+        'Feb': 'фев',
+        'Mar': 'мар',
+        'Apr': 'апр',
+        'May': 'май',
+        'Jun': 'июн',
+        'Jul': 'июл',
+        'Aug': 'авг',
+        'Sep': 'сен',
+        'Oct': 'окт',
+        'Nov': 'ноя',
+        'Dec': 'дек'
+    };
+    
+    const weekdays = {
+        'Sun': 'вс',
+        'Mon': 'пн',
+        'Tue': 'вт',
+        'Wed': 'ср',
+        'Thu': 'чт',
+        'Fri': 'пт',
+        'Sat': 'сб'
+    };
+
+    const monthStr = date.toLocaleString('en', { month: 'short' });
+    const weekdayStr = date.toLocaleString('en', { weekday: 'short' });
+    const day = date.getDate();
+    const time = date.toLocaleTimeString();
+
+    return [`${time}`, `${weekdays[weekdayStr]}, ${day} ${months[monthStr]}`];
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const dateElement = document.getElementById('date');
+    const timeElement = document.getElementById('time');
+    dateElement.textContent = getDate()[1];
+    timeElement.textContent = getDate()[0];
+    if (dateElement) {
+        setInterval(() => {
+            dateElement.textContent = getDate()[1];
+            timeElement.textContent = getDate()[0];
+        }, 1000);
+    }
+});
